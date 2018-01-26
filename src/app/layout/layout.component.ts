b91f3ec705cb866ea9a08b3e10f2e12ea935d3af
taoqun <taoqun@317hu.com>
2018-01-26 09:58:07 +0800

    mock数据添加

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
 
   menu: Array<any> = [
-    { name: '工作站', img: 'anticon-home' child: [{ name: '我的首页', url: '/' }] },
+    { name: '工作站', img: 'anticon-home', child: [{ name: '我的首页', url: '/' }] },
     { name: '患者档案', img: 'anticon-user', child: [{ name: '患者查询', url: '/search' }, { name: '患者主索引', url: '/person' }] },
-    { name: '患者管理', img: 'anticon-setting' child: [{ name: '患者管理', url: '/manange' }, { name: '推介患者', url: '/todo' }] }
+    { name: '患者管理', img: 'anticon-setting', child: [{ name: '患者管理', url: '/manange' }, { name: '推介患者', url: '/todo' }] }
   ];
   constructor() { }
 
