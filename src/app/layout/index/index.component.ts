27303897a14540e2290b0e74fb84088e49f6ed20
qingxo <qingxo@sina.com>
2018-02-09 13:40:17 +0800

    声网视频sdk的集成，未完成

--- src/app/layout/index/index.component.ts
+++ src/app/layout/index/index.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Infos from '../../shared/Infos';
+declare var AgoraRTC: any;
 
 @Component({
   selector: 'app-index',
@@ -6,16 +8,185 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-  title = 'app';
-  domain: String = 'meet.jit.si';
-  options: any;
-  api: any;
+  @Input() channel: any = '1000';
+
+  appid: any = Infos.appid;
   meetingWidth: Number = 700;
   meetingHeight: Number = 700;
+  client = AgoraRTC.createClient({ mode: 'interop' });
+  localStream: any;
+  gohost: Boolean = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeSize(target) {
+    const str = target.target.classList.value;
+    if (str.includes('pos-abs')) {
+      const mt = document.getElementById('meet');
+      const rmt = document.getElementById('remote-meet');
+      mt.className.includes('pos-abs') ? mt.className = 'meet-big' : mt.className = 'meet-small pos-abs';
+      rmt.className.includes('pos-abs') ? rmt.className = 'meet-big' : rmt.className = 'meet-small pos-abs';
+
+    }
+    const sm = document.getElementsByClassName('pos-abs');
 
   }
 
+  initMeeting() {
+    this.initClient();
+  }
+
+  closeMeeting() {
+    this.leave();
+  }
+
+  initClient() {
+    // 初始化 Client 对象
+    /*
+    client.init
+    @param: appid - appid
+    @param: callback - success callback
+    return: null
+    */
+    this.client.init(this.appid, () => {
+      console.log('AgoraRTC client initialized ');
+      this.client.join(null, this.channel, undefined, (uid) => {
+        console.log('User ' + uid + ' join channel successfully');
+        console.log('Timestamp: ' + Date.now());
+        const options: any = {
+          streamID: uid,
+          audio: true,
+          video: true,
+          screen: false,
+          extensionId: 'minllpmhdgpndnkomcoccfekfegnlikg'
+        };
+
+        this.initStream(options);
+      });
+    });
+
+    this.initEvent();
+  }
+
+  initStream(options) {
+    // 创建本地流, 修改对应的参数可以指定启用/禁用特定功能
+    this.localStream = AgoraRTC.createStream(options);
+    this.localStream.setVideoProfile('480p_4');
+    this.localStream.init(() => {
+      console.log('Local stream initialized');
+      this.initPlay();
+      if (this.gohost) {
+        this.client.on('stream-published', function (evt) {
+          console.log('Publish local stream successfully');
+        });
+
+        this.client.publish(this.localStream, function (err) {
+          console.log('Publish stream failed', err);
+        });
+      }
+
+    });
+
+
+  }
+
+  initEvent() {
+    // 监听流事件
+    /*
+    client.on
+    @param: listen to stream event
+    @param: callback listener callback
+    @return: null
+    @event: stream-added when new stream added to channel
+    */
+
+    /*
+    client.subscribe
+    @param: stream stream to subscribe
+    @param: callback failing callback
+    @return: null
+    */
+    this.client.on('stream-added', (evt) => {
+      const stream = evt.stream;
+      console.log('New stream added: ' + stream.getId());
+      console.log('Timestamp: ' + Date.now());
+      console.log('Subscribe ', stream);
+      // 在有新的流加入后订阅远端流
+      this.client.subscribe(stream, function (err) {
+        console.log('Subscribe stream failed', err);
+      });
+    });
+
+    /*
+    @event: peer-leave when existing stream left the channel
+    */
+    this.client.on('peer-leave', function (evt) {
+      console.log('Peer has left: ' + evt.uid);
+      console.log('Timestamp: ' + Date.now());
+      console.log(evt);
+    });
+
+    /*
+    @event: stream-subscribed when a stream is successfully subscribed
+    */
+    this.client.on('stream-subscribed', (evt) => {
+      const stream = evt.stream;
+      console.log('Got stream-subscribed event');
+      this.delRemoteChild();
+      stream.play('remote-meet');
+    });
+
+    /*
+    @event: stream-removed when a stream is removed
+    */
+    this.client.on('stream-removed', function (evt) {
+      const stream = evt.stream;
+      console.log('Stream removed: ' + evt.stream.getId());
+      console.log('Timestamp: ' + Date.now());
+      console.log(evt);
+    });
+    // this.initPlay();
+  }
+
+  initPlay() {
+    // 将本地流在id为meet的dom中播放
+    this.client.publish(this.localStream, function (err) {
+      console.log('Publish local stream error: ' + err);
+    });
+
+    this.client.on('stream-published', function (evt) {
+      console.log('Publish local stream successfully');
+    });
+    this.localStream.play('meet');
+  }
+
+  delRemoteChild() {
+    const div = document.getElementById('remote-meet');
+    while (div.hasChildNodes()) {
+      div.removeChild(div.firstChild);
+    }
+  }
+
+  leave() {
+    this.client.leave(() => {
+      console.log('Leavel channel successfully');
+      this.delRemoteChild();
+    }, function (err) {
+      console.log('Leave channel failed');
+    });
+  }
+
+  publish() {
+    this.client.publish(this.localStream, function (err) {
+      console.log('Publish local stream error: ' + err);
+    });
+  }
+
+  unpublish() {
+    this.client.unpublish(this.localStream, function (err) {
+      console.log('Unpublish local stream failed' + err);
+    });
+  }
 }
