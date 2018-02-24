51819f7e4e74ead48f2edd62ce41641c731fd5f5
qingxo <qingxo@sina.com>
2018-02-24 09:11:14 +0800

    对全局http的header添加，增加了一个过滤

--- src/app/shared/base.http.interceptor.ts
+++ src/app/shared/base.http.interceptor.ts
@@ -5,7 +5,11 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/c
 @Injectable()
 export class BaseHttpInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const authReq = req.clone({ headers: req.headers.set('Content-Type', 'application/x-www-form-urlencoded') });
+    const flag = req.headers.get('customer-type');
+    let authReq = req.clone();
+    if (undefined === flag || 'true' !== flag) {
+      authReq = req.clone({ headers: req.headers.set('Content-Type', 'application/x-www-form-urlencoded') });
+    }
     return next.handle(authReq);
   }
 }
